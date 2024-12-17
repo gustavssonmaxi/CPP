a1d34bfbbe198d0691d142d13febe7872d49d367
gustavssonmaxi <macke.gustavsson@gmail.com>
2024-12-17 18:50:12 +0100

    .

--- dijkstra.cc
+++ dijkstra.cc
@@ -9,12 +9,12 @@ Alla noder förutsätts, när funktionen anropas, ha ett värde som är
 större än alla faktiska avstånd i grafen.
 */
 void dijkstra(Node* start){
-    start->setValue(0);                             //Sätter startnodes längd till sig själv till 0
-    NodeSet dijkstraNodes{};                        //Skapar en tom nodeset
-    dijkstraNodes.add(start);                       //Lägger till startnoden till nodeset
-    while(!dijkstraNodes.isEmpty()){            //Kör tills alla noder bearbetats och fått nya värden relativt startnoden
-        Node *node = dijkstraNodes.removeMin();     //Tar ut en nod att arbeta med
-        for(Edge edge : node->getEdges()){          //Kör dijkstraalgoritmen för en nods alla edges mot andra noder
+    start->setValue(0);                                     //Sätter startnodes längd till sig själv till 0
+    NodeSet dijkstraNodes{};                                //Skapar en tom nodeset
+    dijkstraNodes.add(start);                               //Lägger till startnoden till nodeset
+    while(!dijkstraNodes.isEmpty()){                        //Kör tills alla noder bearbetats och fått nya värden relativt startnoden
+        Node *node = dijkstraNodes.removeMin();             //Tar ut en nod att arbeta med
+        for(Edge edge : node->getEdges()){                  //Kör dijkstraalgoritmen för en nods alla edges mot andra noder
             int length = edge.getLength();                  //Tar ut längden för en edge
             Node *destNode = edge.getDestination();         //Tar ut dess destination
             int newLength = node->getValue() + length;      //Räknar ut dess värde relativt startnoden
