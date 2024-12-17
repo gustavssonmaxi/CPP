b8114482895c983221871ec976dbad088ba4a132
gustavssonmaxi <macke.gustavsson@gmail.com>
2024-12-17 20:54:03 +0100

    main

--- main.cc
+++ main.cc
@@ -1,34 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <string>
-// #include "Node.h"
-// #include "Graph.h" 
-// #include "dijkstra.h" 
+#include "Node.h"
+#include "Graph.h" 
+#include "dijkstra.h" 
 
 
 int main(){
+    Graph graph("graph.txt");
 
-    // Graph graph("graph.txt");
-
-    std::string startName, endName;
+    std::string startNode, endNode;
     std::cout << "Ange startnod: ";
-    std::cin >> startName;
+    std::cin >> startNode;
     std::cout << "Ange destinationsnod: ";
-    std::cin >> endName;
+    std::cin >> endNode;
 
-/*  Node* start = graph.findNode(startNode);
-    Node* end = graph.findNode(endNode);
+    Node* start = graph.find(startNode);
+    Node* end = graph.find(endNode);
 
     if (!start || !end) {
         std::cerr << "Ogiltig start- eller slutnod\n";
         return 1;
     }
 
-    graph.resetNodes();
+    graph.resetVals();
     dijkstra(start);
 
     std::cout << "Kortaste avstånd från " << startNode << " till " << endNode << ": " << end->getValue() << "\n";
     return 0;
-    */
 }
 
